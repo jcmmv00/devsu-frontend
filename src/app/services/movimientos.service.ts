5cbd4c715188697768c6d4c58435afbf2701ef24
Joseph M <jcmmv00@gmail.com>
2024-09-18 22:54:28 -0500

    Dockerfile and cors

--- src/app/services/movimientos.service.ts
+++ src/app/services/movimientos.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { apiUrl } from '../environment';
@@ -21,6 +21,22 @@ export class MovimientosService {
 
   createMovimiento(movimiento: Movimiento): Observable<any> {
     return this.http.post<Movimiento>(this.apiURLMovimientos, movimiento);
+  }
+
+  formatDate(date: Date): string {
+    const day = ('0' + date.getDate()).slice(-2);
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const year = date.getFullYear();
+    return `${day}-${month}-${year}`;
+  }
+
+  obtenerReporte(fechaInicial: Date, fechaFinal: Date, accountNumber: string) {
+    const formattedStartDate = this.formatDate(fechaInicial);
+    const formattedEndDate = this.formatDate(fechaFinal);
+    const params = new HttpParams()
+      .set('date', `${formattedStartDate},${formattedEndDate}`)
+      .set('accountNumber', accountNumber);
+    return this.http.get<any>(this.apiURLMovimientos + '/report', { params });
   }
 
 }
