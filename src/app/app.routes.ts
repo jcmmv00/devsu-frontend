8259c88db92f403326b6eaf5c5457f4ed91b5ce1
Joseph M <jcmmv00@gmail.com>
2024-09-18 19:10:55 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ClientesComponent } from './clientes/clientes.component';
+import { CuentasComponent } from './cuentas/cuentas.component';
+import { ReportesComponent } from './reportes/reportes.component';
+import { MovimientosComponent } from './movimientos/movimientos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/clientes', pathMatch: 'full' }, // Ruta por defecto
+    { path: 'clientes', component: ClientesComponent },
+    { path: 'cuentas', component: CuentasComponent },
+    { path: 'movimientos', component: MovimientosComponent },
+    { path: 'reportes', component: ReportesComponent }
+];
